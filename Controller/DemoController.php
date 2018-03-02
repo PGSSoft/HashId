0611e9e6c7df5613d1e2da7460324067850152be
Karol Jonski <kjonski@pgs-soft.com>
2018-03-02 12:54:05 +0100

    Rebuild DemoController

--- Controller/DemoController.php
+++ Controller/DemoController.php
@@ -7,20 +7,25 @@ namespace Pgs\HashIdBundle\Controller;
 use Pgs\HashIdBundle\Annotation\Hash;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
 
 class DemoController extends Controller
 {
-    /**
-     * @Route("/hash-id/demo/{id}", name="pgs_hash_id_demo")
-     * @Hash("id")
-     */
-    public function demo($id)
+    public function encode($id)
     {
-        $url = $this->generateUrl('pgs_hash_id_demo', ['id' => $id]);
+        $url = $this->generateUrl('pgs_hash_id_demo_decode', ['id' => $id]);
 
         return new Response(
             '<html><body>Provided id: '.$id.'<br />url: '.$url.'</body></html>'
+        );
+    }
+
+    /**
+     * @Hash("id")
+     */
+    public function decode($id)
+    {
+        return new Response(
+            '<html><body>Decoded id: '.$id.'<br /></body></html>'
         );
     }
 }
