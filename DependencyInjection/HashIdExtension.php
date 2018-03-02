9fded40e1360574b9c1c7bdc4bbfa9c9eb8d3bce
Karol Jonski <kjonski@pgs-soft.com>
2018-03-02 12:59:46 +0100

    Register Hash annotation

--- DependencyInjection/HashIdExtension.php
+++ DependencyInjection/HashIdExtension.php
@@ -4,6 +4,8 @@
 namespace Pgs\HashIdBundle\DependencyInjection;
 
 
+use Doctrine\Common\Annotations\AnnotationRegistry;
+use Pgs\HashIdBundle\Annotation\Hash;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
@@ -24,6 +26,14 @@ class HashIdExtension extends Extension
         $container->setParameter('pgs_hash_id.salt', $config['salt']);
         $container->setParameter('pgs_hash_id.min_hash_length', $config['min_hash_length']);
         $container->setParameter('pgs_hash_id.alphabet', $config['alphabet']);
+
+        $this->registerAnnotation();
     }
+
+    private function registerAnnotation(): void
+    {
+        AnnotationRegistry::loadAnnotationClass(Hash::class);
+    }
+
 
 }
