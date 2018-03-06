686c6083fea3e81bd6a5c57cc3d333d59b778d5a
Karol Jonski <kjonski@pgs-soft.com>
2018-03-06 14:42:01 +0100

    Improve demo

--- Controller/DemoController.php
+++ Controller/DemoController.php
@@ -13,20 +13,51 @@ class DemoController extends Controller
 {
     public function encode($id)
     {
-        $url = $this->generateUrl('pgs_hash_id_demo_decode', ['id' => $id]);
+        $other = 30;
+        $url1 = $this->generateUrl('pgs_hash_id_demo_decode', ['id' => $id, 'other' => $other]);
+        $url2 = $this->generateUrl('pgs_hash_id_demo_decode_more', ['id' => $id, 'other' => $other]);
 
-        return new Response(
-            '<html><body>Provided id: ' . $id . '<br />Url with encoded parameter: <a href="' . $url . '">' . $url . '</a></body></html>'
-        );
+        $response = <<<EOT
+            <html>
+                <body>
+                Provided id: $id, other: $other <br />
+                Url with encoded parameter: <a href="$url1">$url1</a><br />
+                Another url with encoded more parameters: <a href="$url2">$url2</a><br />
+                </body>
+            </html>
+EOT;
+        return new Response($response);
     }
 
     /**
      * @Hash("id")
      */
-    public function decode(Request $request, $id)
+    public function decode(Request $request, int $id, int $other)
     {
-        return new Response(
-            '<html><body>Provided id: ' . $request->attributes->get('_route_params')['id'] . '<br />Decoded id: ' . $id . '<br /></body></html>'
-        );
+        return new Response($this->getDecodeResponse($request, $id, $other));
+    }
+
+    /**
+     * @Hash({"id", "other"})
+     */
+    public function decodeMore(Request $request, int $id, int $other)
+    {
+        return new Response($this->getDecodeResponse($request, $id, $other));
+    }
+
+    private function getDecodeResponse(Request $request, int $id, int $other)
+    {
+        $providedId = $request->attributes->get('_route_params')['id'];
+        $providedOther = $request->attributes->get('_route_params')['other'];
+
+        $response = <<<EOT
+            <html>
+                <body>
+                Provided id: <b>$providedId</b>, other: <b>$providedOther</b><br />
+                Decoded id: <b>$id</b>, other: <b>$other</b><br />
+                </body>
+            </html>
+EOT;
+        return $response;
     }
 }
