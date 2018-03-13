106afe1841842d2a05386c5121e3e0a65439186b
Karol Jonski <kjonski@pgs-soft.com>
2018-03-13 11:49:58 +0100

    Change directory structure

--- bootstrap.php
+++
@@ -1,6 +0,0 @@
-<?php
-require __DIR__.'/Tests/Reflection/Fixtures/ExistingClass.php';
-require __DIR__.'/Traits/DecoratorTrait.php';
-require __DIR__.'/Tests/Traits/Fixtures/BaseTestClass.php';
-require __DIR__.'/Tests/Traits/Fixtures/DecorateTestClass.php';
-require __DIR__.'/../../autoload.php';
---
+++ tests/bootstrap.php
@@ -0,0 +1,6 @@
+<?php
+require __DIR__.'/Reflection/Fixtures/ExistingClass.php';
+require __DIR__.'/../src/Traits/DecoratorTrait.php';
+require __DIR__.'/Traits/Fixtures/BaseTestClass.php';
+require __DIR__.'/Traits/Fixtures/DecorateTestClass.php';
+require __DIR__.'/../../../autoload.php';
