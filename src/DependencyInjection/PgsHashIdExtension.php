f1892f50a492be517fb2fe804c5f0621091a2790
Karol Jonski <kjonski@pgs-soft.com>
2020-01-27 22:30:05 +0100

    Use xml configuration file

--- src/DependencyInjection/PgsHashIdExtension.php
+++ src/DependencyInjection/PgsHashIdExtension.php
@@ -6,18 +6,18 @@ use Doctrine\Common\Annotations\AnnotationRegistry;
 use Pgs\HashIdBundle\Annotation\Hash;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 
 class PgsHashIdExtension extends Extension
 {
     public function load(array $configs, ContainerBuilder $container): void
     {
-        $loader = new YamlFileLoader(
+        $loaderXml = new XmlFileLoader(
             $container,
             new FileLocator(__DIR__.'/../Resources/config')
         );
-        $loader->load('services.yaml');
+        $loaderXml->load('services.xml');
 
         $configuration = new Configuration();
         $config = $this->processConfiguration($configuration, $configs);
